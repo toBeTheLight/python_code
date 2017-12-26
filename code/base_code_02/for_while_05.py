bd4255f0fec6a25d5114f518d5bbc0a5c36fb26d
toBeTheLight <yl1991web@126.com>
2017-12-26 18:29:53 +0800

    update

--- code/base_code_02/for_while_05.py
+++ code/base_code_02/for_while_05.py
@@ -4,7 +4,6 @@
 '''
 
 # for in
-
 for item in ['a', 'b', 'c']:
     print(item)
 print('------------------')
@@ -13,8 +12,8 @@ SUM = 0
 for item in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
     SUM += item
 print(SUM)
-## 有一个range()函数可生成整数序列
 
+## 有一个range()函数可生成整数序列
 SUM2 = 0
 for item in list(range(11)):
     SUM2 += item
@@ -22,8 +21,43 @@ print(SUM2)
 
 SUM3 = 0
 print(list(range(10, 20, 3)))
+
 # 从10 到 25 间隔为5
 for item in list(range(10, 25, 5)):
     SUM3 += item
 print(SUM3)
+print('------------------')
+
+
+# while 
+SUM = 0
+NTH = 99
+while NTH > 0:
+    NTH -= 2
+    SUM += NTH
+print(SUM)
+print('------------------')
+
+# break 提前结束循环
+SUM = 0
+NTH = 99
+while NTH > 0:
+    NTH -= 2
+    SUM += NTH
+    if NTH < 50:
+        break
+print(SUM)
+print('------------------')
+
+# continue
+
+SUM = 0
+NTH = 99
+while NTH > 0:
+    NTH -= 2
+    if NTH == 51:
+        print('跳过：', NTH)
+        continue
+    SUM += NTH
+print(SUM)
 print('------------------')
